381519dcf38606ccab8bba88567796715b0ee709
Zenitram <Zenitram@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2012-10-24 07:37:35 +0000

    + Cleanup and optimizations, thanks to XhmikosR

--- MediaInfo_Website/trunk/HomePage/MediaInfo/_/minify.bat
+++ MediaInfo_Website/trunk/HomePage/MediaInfo/_/minify.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 
-cleancss -o CSS/Default.min.css CSS/Default.css && ^
+cleancss -o CSS/Style.min.css CSS/Style.css && ^
 cleancss -o CSS/Tags.min.css CSS/Tags.css && ^
 uglifyjs -o JavaScript/Utils.min.js JavaScript/Utils.js && ^
 uglifyjs -o JavaScript/Tags.min.js JavaScript/Tags.js
