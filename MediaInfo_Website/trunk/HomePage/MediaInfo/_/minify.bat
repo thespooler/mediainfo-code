27fb9e70de65dc3f216a9bce394b57974ff3aa86
Zenitram <Zenitram@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2012-11-15 11:49:08 +0000

    + Cleanup and optimizations, thanks to XhmikosR

--- MediaInfo_Website/trunk/HomePage/MediaInfo/_/minify.bat
+++ MediaInfo_Website/trunk/HomePage/MediaInfo/_/minify.bat
@@ -1,7 +1,13 @@
 @echo off
+rem install node.js and then run:
+rem npm install -g clean-css
+rem npm install -g uglify-js
+
 pushd %~dp0
 
-cleancss -o CSS/Style.min.css CSS/Style.css && ^
-cleancss -o CSS/Tags.min.css CSS/Tags.css && ^
-uglifyjs -o JavaScript/Utils.min.js JavaScript/Utils.js && ^
-uglifyjs -o JavaScript/Tags.min.js JavaScript/Tags.js
+echo minifying and combining css and js files...
+type CSS\normalize.css CSS\Style.css CSS\Tags.css | cleancss --s0 -o css\pack.css
+cmd /c uglifyjs -o JavaScript/Utils.min.js JavaScript/Utils.js && ^
+cmd /c uglifyjs -o JavaScript/Tags.min.js JavaScript/Tags.js
+
+popd
