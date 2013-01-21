ab67d61a084c89092083fd0758b02b4608dab805
zenitram <zenitram@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2013-01-21 09:16:11 +0000

    + Cleanup and optimizations, thanks to XhmikosR

--- MediaInfo_Website/trunk/HomePage/MediaInfo/_/minify.bat
+++ MediaInfo_Website/trunk/HomePage/MediaInfo/_/minify.bat
@@ -7,6 +7,6 @@ pushd %~dp0
 
 echo minifying and combining css and js files...
 type CSS\normalize.css CSS\Style.css CSS\Tags.css | cleancss --s0 -o CSS\pack.css
-cmd /c uglifyjs JavaScript\Utils.js JavaScript\Tags.js --compress --mangle -o JavaScript\pack.js
+cmd /c uglifyjs JavaScript\Tags.js --compress --mangle -o JavaScript\pack.js
 
 popd
