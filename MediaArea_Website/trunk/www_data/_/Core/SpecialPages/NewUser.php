dd6d2673b94cc530bf38b1e4982f2b66944bae0e
zenitram <zenitram@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2013-06-29 15:41:11 +0000

    From MediaInfo website

---
+++ MediaArea_Website/trunk/www_data/_/Core/SpecialPages/NewUser.php
@@ -0,0 +1,16 @@
+<?php
+//-Special case : new.html
+if (empty($Page) || $Page=="/" || $Page=="/MediaInfo") {
+    header ("Location: http://".$_SERVER['HTTP_HOST']."/".$Language."/MediaInfo");
+    exit;
+}
+
+//-Special case : is a directory
+if ($Page{strlen($Page)-1}=="/") {
+    $Page=rtrim($Page, "/");
+    header ("HTTP/1.1 301 Moved Permanently");
+	header ("Location: http://".$_SERVER['HTTP_HOST'].$Page);
+    exit;
+}
+
+?>
