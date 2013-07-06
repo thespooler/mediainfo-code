f2e9f233bbc5c6b569b66a0671282fdaca6c878f
xhmikosr <xhmikosr@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2013-07-06 09:06:15 +0000

    Move minify.bat in root dir.

--- MediaArea_Website/trunk/minify.bat
+++ MediaArea_Website/trunk/minify.bat
@@ -5,8 +5,12 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
+pushd "www\_"
+
 echo minifying and combining css and js files...
 type css\bootstrap.css css\normalize.css css\jquery.fancybox.css css\jquery.fancybox-thumbs.css css\Style.css css\Tags.css | cleancss --s0 -e -o css\pack.css
 cmd /c uglifyjs js\jquery.fancybox.js js\jquery.fancybox-thumbs.js js\jquery.mousewheel.js js\Tags.js --compress --mangle -o js\pack.js
+
+popd
 
 popd
