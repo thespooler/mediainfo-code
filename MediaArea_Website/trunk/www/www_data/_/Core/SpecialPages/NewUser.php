53bfbd59d0500c86e9640ab5d2145ef75fc957e8
zenitram <zenitram@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2013-11-22 11:58:14 +0000

    x infinite redirection /en

--- MediaArea_Website/trunk/www/www_data/_/Core/SpecialPages/NewUser.php
+++ MediaArea_Website/trunk/www/www_data/_/Core/SpecialPages/NewUser.php
@@ -8,8 +8,13 @@ if (empty($Page) || $Page=="/" || $Page=="/MediaInfo") {
 //-Special case : is a directory
 if ($Page{strlen($Page)-1}=="/") {
     $Page=rtrim($Page, "/");
+    $PathTemp=$_SERVER["SCRIPT_FILENAME"]; $PathTemp=substr($PathTemp, 0, strrpos($PathTemp, "/")); $PathTemp.=$Page;
+
     header ("HTTP/1.1 301 Moved Permanently");
-    header ("Location: ".($IsSecure?"https://":"http://").$_SERVER['HTTP_HOST'].$Page);
+    if (file_exists($PathTemp))
+        header ("Location: ".($IsSecure?"https://":"http://").$_SERVER['HTTP_HOST']); //Should not access it directly
+    else
+        header ("Location: ".($IsSecure?"https://":"http://").$_SERVER['HTTP_HOST'].$Page);
     exit;
 }
 
