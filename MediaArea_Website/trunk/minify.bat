604dba4c494d2aa31b1028039d36e5f9e5f50782
xhmikosr <xhmikosr@45c5ae6f-87cc-4fd0-92ee-e4f023fd80da>
2014-02-15 06:37:06 +0000

    Update to the latest clean-css.

--- MediaArea_Website/trunk/minify.bat
+++ MediaArea_Website/trunk/minify.bat
@@ -8,7 +8,7 @@ pushd %~dp0
 pushd "www\_"
 
 echo minifying and combining css and js files...
-type css\normalize.css css\bootstrap.css css\jquery.fancybox.css css\jquery.fancybox-thumbs.css css\Style.css css\Tags.css | cleancss --s0 --selectors-merge-mode ie8 -o css\pack.css
+type css\normalize.css css\bootstrap.css css\jquery.fancybox.css css\jquery.fancybox-thumbs.css css\Style.css css\Tags.css | cleancss --s0 --compatibility ie8 -o css\pack.css
 cmd /c uglifyjs js\bootstrap.js js\jquery.fancybox.js js\jquery.fancybox-thumbs.js js\jquery.mousewheel.js js\Tags.js --compress --mangle -o js\pack.js
 
 popd
